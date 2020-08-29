2df15bd28cec5d1b51bae7772d5b4cd527ab06f5
Darshanjina <darshanjina1111998@gmail.com>
2020-08-29 10:54:17 +0530

    initial commit

--- src/action/type.js
+++ src/action/type.js
@@ -1,4 +1,4 @@
 export const GET_ALL_CATEGORY = "GET_ALL_CATEGORY";
 export const GET_ALL_POST = "GET_ALL_POST";
 export const GET_SORTED_POST = "GET_SORTED_POST"; 
-export const API = 'http://localhost:3000/api/';
+export const API = "https://explore-bike-server.herokuapp.com/api";
